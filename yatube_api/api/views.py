a2ecadc99e64589f276fe44571c1cd3d4512f32e
Belyaev Vadim <a3unnn@gmail.com>
2025-04-15 16:28:45 +0500

    version2

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,7 +1,5 @@
 from rest_framework import viewsets
-from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 
 from posts.models import Post, Group, Comment
 
